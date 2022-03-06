4f2cd24bd85a2b0eb1193fa2375e205642832eba
luckyAkbar <luckyakbar1509@gmail.com>
2022-03-06 07:31:06 +0700

    open new route to get and create list absent

--- router/router.go
+++ router/router.go
@@ -4,17 +4,22 @@ import (
 	"himatro-api/handler"
 	"himatro-api/middleware"
 
+	echoMiddleware "github.com/labstack/echo/v4/middleware"
+
 	"github.com/labstack/echo/v4"
 )
 
 func Router() *echo.Echo {
 	e := echo.New()
 
+	e.Use(echoMiddleware.Logger())
+
 	e.GET("/", handler.HomeGet)
-	e.GET("/list_absensi", handler.ListAbsensi)
+	e.GET("/absensi/:absentID", handler.GetAbsentList)
 	e.POST("/login", handler.Login)
 
-	e.GET("/admin", handler.Admin, middleware.RequireLogin)
+	e.GET("/admin", handler.Admin)
+	e.POST("/admin/absensi", handler.InitAbsent, middleware.RequireLogin)
 
 	return e
 }
