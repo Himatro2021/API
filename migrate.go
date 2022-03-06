94c8a6943b56cb13020cd3eede2ae00e24ae3292
luckyAkbar <luckyakbar1509@gmail.com>
2022-03-06 07:29:12 +0700

    change models name of AbsensiPengurus to AbsentList

--- migrate.go
+++ migrate.go
@@ -12,7 +12,7 @@ func Migrate() {
 	db.DB.AutoMigrate(&models.Jabatan{})
 	db.DB.AutoMigrate(&models.Pengurus{})
 	db.DB.AutoMigrate(&models.FormAbsensi{})
-	db.DB.AutoMigrate(&models.AbsensiPengurus{})
+	db.DB.AutoMigrate(&models.AbsentList{})
 	db.DB.AutoMigrate(&models.Departemen{})
 	db.DB.AutoMigrate(&models.User{})
 }
