e8f205830f32b0184e5477319510057e3737e79b
luckyAkbar <luckyakbar1509@gmail.com>
2022-03-13 15:24:40 +0700

    initial commit

---
+++ controller/fillAbsentForm.go
@@ -0,0 +1,97 @@
+package controller
+
+import (
+	"errors"
+	"fmt"
+	"himatro-api/db"
+	"himatro-api/models"
+	"time"
+)
+
+func FillAbsentForm(absentID int, NPM string, keterangan string) error {
+	pengurus, err := getPengurusData(NPM)
+
+	if err != nil {
+		return err
+	}
+
+	formDetail, err := getFormAbsentDetail(absentID)
+
+	if err != nil {
+		return err
+	}
+
+	if formDetail.Participant != pengurus.DepartemenID && formDetail.Participant != 0 {
+		return fmt.Errorf("you are not the expected attendance of this absent form")
+	}
+
+	saveAttendanceRecord(absentID, NPM, keterangan)
+
+	return nil
+}
+
+func IsFormWriteable(absentID int) error {
+	formAbsensi, err := getFormAbsentDetail(absentID)
+
+	if err != nil {
+		return err
+	}
+
+	if formAbsensi.StartAt.After(time.Now()) {
+		return fmt.Errorf("absent form with ID: %d is not open yet", absentID)
+	}
+
+	if formAbsensi.FinishAt.Before(time.Now()) {
+		return fmt.Errorf("absent form with ID: %d is already closed", absentID)
+	}
+
+	return nil
+}
+
+func getFormAbsentDetail(absentID int) (models.FormAbsensi, error) {
+	formAbsent := models.FormAbsensi{}
+
+	res := db.DB.Model(&models.FormAbsensi{}).
+		Where("id = ?", absentID).
+		First(&formAbsent)
+
+	if res.Error != nil {
+		return models.FormAbsensi{}, fmt.Errorf("absent form with ID: %d is not exists", absentID)
+	}
+
+	return formAbsent, nil
+}
+
+func getPengurusData(NPM string) (models.Pengurus, error) {
+	pengurus := models.Pengurus{
+		NPM: NPM,
+	}
+
+	res := db.DB.Model(&models.Pengurus{}).
+		Where(&models.Pengurus{
+			NPM: NPM,
+		}).
+		First(&pengurus)
+
+	if res.Error != nil {
+		return pengurus, fmt.Errorf("pengurus with NPM: %s is not found", NPM)
+	}
+
+	return pengurus, nil
+}
+
+func saveAttendanceRecord(absentID int, NPM string, keterangan string) error {
+	res := db.DB.Model(&models.AbsentList{}).
+		Where(&models.AbsentList{
+			FormAbsensiID: uint(absentID),
+			NPM:           NPM,
+		}).
+		Update("keterangan", keterangan)
+
+	if res.Error != nil {
+		return errors.New("system failure to save absent record")
+	}
+
+	return nil
+
+}
---
+++ handler/fillAbsentForm.go
@@ -0,0 +1,74 @@
+package handler
+
+import (
+	"fmt"
+	"himatro-api/controller"
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+)
+
+func CheckAbsentForm(c echo.Context) error {
+	absentID, err := strconv.Atoi(c.Param("absentID"))
+
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, ErrorMessage{
+			OK:      false,
+			Message: "Absent ID must be a valid numeric string.",
+		})
+	}
+
+	if err := controller.IsFormWriteable(absentID); err != nil {
+		return c.JSON(http.StatusForbidden, ErrorMessage{
+			OK:      false,
+			Message: fmt.Sprintf("Request forbidden because: %s", err.Error()),
+		})
+	}
+
+	return c.NoContent(http.StatusOK)
+}
+
+func FillAbsentForm(c echo.Context) error {
+	absentID, err := strconv.Atoi(c.Param("absentID"))
+
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, ErrorMessage{
+			OK:      false,
+			Message: "Absent ID must be a valid numeri string.",
+		})
+	}
+
+	if err := controller.IsFormWriteable(absentID); err != nil {
+		return c.JSON(http.StatusBadRequest, ErrorMessage{
+			OK:      false,
+			Message: fmt.Sprintf("Failed to fill absent form because: %s", err.Error()),
+		})
+	}
+
+	NPM := c.FormValue("NPM")
+	keterangan := c.FormValue("keterangan")
+
+	if NPM == "" || keterangan == "" {
+		return c.JSON(http.StatusBadRequest, ErrorMessage{
+			OK:      false,
+			Message: "All required field must be supplied.",
+		})
+	}
+
+	if keterangan != "h" && keterangan != "i" {
+		return c.JSON(http.StatusBadRequest, ErrorMessage{
+			OK:      false,
+			Message: fmt.Sprintf("Value keterangan: %s is invalid.", keterangan),
+		})
+	}
+
+	if err := controller.FillAbsentForm(absentID, NPM, keterangan); err != nil {
+		return c.JSON(http.StatusBadRequest, ErrorMessage{
+			OK:      false,
+			Message: fmt.Sprintf("Failed to fill absent form because: %s", err.Error()),
+		})
+	}
+
+	return nil
+}
