37a9b47a54e9511074e4a0e4d1c656092aa3772f
luckyAkbar <luckyakbar1509@gmail.com>
2022-03-23 08:25:10 +0700

    implement binding and validator in json payload

--- internal/handler/initAbsent.go
+++ internal/handler/initAbsent.go
@@ -1,15 +1,33 @@
 package handler
 
 import (
-	"fmt"
+	"himatro-api/internal/contract"
 	"himatro-api/internal/controller"
+	"himatro-api/internal/util"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
 func InitAbsent(c echo.Context) error {
-	initAbsentPayload, err := controller.ExtractInitAbsentPayload(c)
+	createAbsentPayload := contract.CreateAbsentForm{}
+
+	if err := c.Bind(&createAbsentPayload); err != nil {
+		return c.JSON(http.StatusBadRequest, ErrorMessage{
+			OK:      false,
+			Message: "Invalid type of JSON payload received",
+		})
+	}
+
+	if err := util.Validator.Struct(&createAbsentPayload); err != nil {
+		return c.JSON(http.StatusOK, JSONPayloadValidationError{
+			OK:      false,
+			Message: "JSON payload validation error",
+			Details: util.ExtractValidationErrorMsg(err),
+		})
+	}
+
+	initAbsentPayload, err := controller.ExtractInitAbsentPayload(createAbsentPayload)
 
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, ErrorMessage{
