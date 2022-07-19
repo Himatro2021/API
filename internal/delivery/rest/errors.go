fc077d7eacf50b3618e4b753e1c9385ee8396ee5
luckyAkbar <lucky.akbar105619@students.unila.ac.id>
2022-07-19 20:59:41 +0700

    add new errors type

--- internal/delivery/rest/errors.go
+++ internal/delivery/rest/errors.go
@@ -8,11 +8,20 @@ import (
 
 var (
 	// ErrBadRequest self explained
-	ErrBadRequest = echo.NewHTTPError(http.StatusBadRequest, "Invalid request. Please send a vaid payload")
+	ErrBadRequest = echo.NewHTTPError(http.StatusBadRequest, "Invalid request. Please send a valid payload")
 
 	// ErrValidation self explained
 	ErrValidation = echo.NewHTTPError(http.StatusBadRequest, "Validation Error")
 
+	// ErrNotFound self explained
+	ErrNotFound = echo.NewHTTPError(http.StatusNotFound, "Record Not Found")
+
 	// ErrInternal self explained
-	ErrInternal = echo.NewHTTPError(http.StatusInternalServerError, "internal server error")
+	ErrInternal = echo.NewHTTPError(http.StatusInternalServerError, "Internal Server Error")
+
+	// ErrForbidden self explained
+	ErrForbidden = echo.NewHTTPError(http.StatusForbidden, "Access Forbidden")
+
+	// ErrAlreadyExists self explained
+	ErrAlreadyExists = echo.NewHTTPError(http.StatusForbidden, "Record already exists!")
 )
