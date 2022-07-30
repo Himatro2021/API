c47b557defd0f410dff9e43fd303639422dc2b6d
luckyAkbar <lucky.akbar105619@students.unila.ac.id>
2022-07-30 22:35:33 +0700

    add new helper function

--- internal/helper/closer.go
+++ internal/helper/closer.go
@@ -8,3 +8,17 @@ func WrapCloser(close func() error) {
 		logrus.Error(err)
 	}
 }
+
+// LogIfErr will log with error level if received non nil error
+func LogIfErr(err error) {
+	if err != nil {
+		logrus.Error(err)
+	}
+}
+
+// PanicIfErr will call logrus.Panic if received non nil error
+func PanicIfErr(err error) {
+	if err != nil {
+		logrus.Panic(err)
+	}
+}
